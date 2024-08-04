f7dfef91c6a1067889f7ec0bfe48fda48cf875d9
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-08-04 09:41:25 +0700

    Update

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/text", (req, res) => {
+app.get("/time", (req, res) => {
   const time = new Date().toLocaleTimeString();
   res.send(`<div>Clicked @ ${time}</div>`);
+});
+
+app.get("/loading", async (req, res) => {
+  await sleep(1000);
+  const time = new Date().toLocaleTimeString();
+  res.send(`<div>Clicked @ ${time}</div>`);
+});
+
+app.post("/form", (req, res) => {
+  const input = req.body?.input ?? "";
+  const time = new Date().toLocaleTimeString();
+  res.send(`<div>Received "${input}" @ ${time}</div>`);
 });
 
 // Running app
@@ -16,3 +28,7 @@ app.listen(PORT, async () => {
   console.log(`Listening on port ${PORT}`);
   console.log(`http://localhost:${PORT}`);
 });
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
